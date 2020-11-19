99efc5451e8031520ec29618b7745a1b09ee86d4
chensy <1278651995@qq.com>
2020-11-19 21:41:46 +0800

    选择排序

---
+++ SoredFunction/SelectionSort.go
@@ -0,0 +1,12 @@
+package SoredFunction
+
+func SelectionSort(nums []int) []int {
+	for i := 0; i < len(nums); i++ {
+		for j := i; j < len(nums); j++ {
+			if nums[i] > nums[j] {
+				nums[i], nums[j] = nums[j], nums[i]
+			}
+		}
+	}
+	return nums
+}
--- main.go
+++ main.go
@@ -8,4 +8,7 @@ import (
 
 func main() {
 	fmt.Println(SoredFunction.QuickSort(Tools.GenerateRandArray(10)))
+	fmt.Println(SoredFunction.MergeSort(Tools.GenerateRandArray(10)))
+	fmt.Println(SoredFunction.BubbleSort(Tools.GenerateRandArray(10)))
+	fmt.Println(SoredFunction.SelectionSort(Tools.GenerateRandArray(10)))
 }
