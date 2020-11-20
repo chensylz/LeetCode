44d4daa0ce71ad1aeb03f4260718a1a1e07f856c
chensy <1278651995@qq.com>
2020-11-20 23:00:52 +0800

    104. 二叉树的最大深度

--- 二叉树/二叉树应用/maximum-depth-of-binary-tree/main.go
+++ 二叉树/二叉树应用/maximum-depth-of-binary-tree/main.go
@@ -1,17 +1,14 @@
 package main
 
+import "GoLeetCode/DataStructure"
+
 /**
 104. 二叉树的最大深度
 https://leetcode-cn.com/problems/maximum-depth-of-binary-tree/
  */
 
-type TreeNode struct {
-	Val int
-	Left *TreeNode
-	Right *TreeNode
-}
 
-func maxDepth(root *TreeNode) int {
+func maxDepth(root *DataStructure.TreeNode) int {
 	if root == nil {
 		return 0
 	}
