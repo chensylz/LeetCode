d8bb059f52d02cb2fbefedd55f5a3a571e2c0a6e
chensy <1278651995@qq.com>
2020-12-22 21:12:10 +0800

    62. 不同路径

---
+++ 动态规划/unique-paths/main.go
@@ -0,0 +1,29 @@
+package unique_paths
+
+/**
+62. 不同路径
+https://leetcode-cn.com/problems/unique-paths/
+ */
+
+func uniquePaths(m int, n int) int {
+	if m == 0 || n == 0 {
+		return 0
+	}
+	dp := make([][]int, m)
+	for i := 0; i < m; i++ {
+		for j := 0; j < n; j++ {
+			if dp[i] == nil {
+				dp[i] = make([]int, n)
+			}
+			dp[i][j] = 1
+		}
+	}
+
+	for i := 1; i < m; i++ {
+		for j := 1; j < n; j++ {
+			dp[i][j] = dp[i - 1][j] + dp[i][j - 1]
+		}
+	}
+
+	return dp[m - 1][n - 1]
+}
