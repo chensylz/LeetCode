d92d9b84aa13c98aa8d70f98fa7558199521753e
chensy <1278651995@qq.com>
2020-12-22 21:12:21 +0800

    120. 三角形最小路径和

--- 动态规划/triangle/main.go
+++ 动态规划/triangle/main.go
@@ -4,17 +4,28 @@ package triangle
 120. 三角形最小路径和
 https://leetcode-cn.com/problems/triangle/
  */
+var memory [][]int
 
-func minimumTotal(triangle [][]int) int {
+func minimumTotal2(triangle [][]int) int {
+	memory = make([][]int, len(triangle))
+	for i := 0; i < len(triangle); i++ {
+		memory[i] = make([]int, len(triangle[len(triangle) - 1]))
+	}
 	return recursion(triangle, 0, 0)
 }
+
+
 
 func recursion(triangle [][]int, i, j int) int {
 	if i == len(triangle) {
 		return 0
 	}
 
-	return min(recursion(triangle, i + 1, j), recursion(triangle, i + 1, j + 1)) + triangle[i][j]
+	if memory[i][j] != 0 {
+		return memory[i][j]
+	}
+	memory[i][j] = min(recursion(triangle, i + 1, j), recursion(triangle, i + 1, j + 1)) + triangle[i][j]
+	return memory[i][j]
 }
 
 func min(x, y int) int {
