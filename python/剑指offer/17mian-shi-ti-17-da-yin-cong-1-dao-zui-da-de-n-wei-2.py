6c17ea73a92ea0c507a49ca4ae50b7a169362a99
chensy <1278651995@qq.com>
2021-02-25 19:00:09 +0800

    剑指 Offer 17. 打印从1到最大的n位数

---
+++ python/剑指offer/17mian-shi-ti-17-da-yin-cong-1-dao-zui-da-de-n-wei-2.py
@@ -0,0 +1,12 @@
+# coding=utf8
+
+"""
+剑指 Offer 17. 打印从1到最大的n位数
+https://leetcode-cn.com/problems/da-yin-cong-1dao-zui-da-de-nwei-shu-lcof/
+"""
+from typing import List
+
+
+class Solution:
+    def printNumbers(self, n: int) -> List[int]:
+        return list(range(1, 10 ** n))
