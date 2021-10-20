24f8e53d110690b7e482453e788953f3950717e3
chenjieping <chenjieping@kezaihui.com>
2021-10-20 16:40:17 +0800

    977.squares-of-a-sorted-array(2)

--- golang/sequence/0977_squares-of-a-sorted-array/squares-of-a-sorted-array.go
+++ golang/sequence/0977_squares-of-a-sorted-array/squares-of-a-sorted-array.go
@@ -14,14 +14,39 @@ example1:
 解法1:
 	平方后排序，时间复杂度O(nlogn)
 解法2:
-	找到中间值，
+	找到中间值，然后中间值的两侧平方后都是递增的，判断两侧值依次插入结果数组即可
  */
 
-func sortedSquares(nums []int) []int {
+func sortedSquares1(nums []int) []int {
 	result := make([]int, len(nums))
 	for index, value := range nums {
 		result[index] = value * value
 	}
 	sort.Ints(result)
 	return result
+}
+
+func sortedSquares(nums []int) []int {
+	n := len(nums)
+	midIndex := -1
+	for i := 0; i < n && nums[i] < 0; i++ {
+		midIndex = i
+	}
+	ans := make([]int, 0, n)
+	for i, j := midIndex, midIndex + 1; i >= 0 || j < n; {
+		if i < 0 {
+			ans = append(ans, nums[j] * nums[j])
+			j++
+		} else if j == n {
+			ans = append(ans, nums[i] * nums[i])
+			i--
+		} else if nums[i] * nums[i] < nums[j] * nums[j] {
+			ans = append(ans, nums[i] * nums[i])
+			i--
+		} else {
+			ans = append(ans, nums[j] * nums[j])
+			j++
+		}
+	}
+	return ans
 }
