b5838cb6823b6b26d502eef42a63d2abdc119556
1278651995 <1278651995@qq.com>
2022-01-04 22:52:02 +0800

    美好的开始001

---
+++ golang/current/001_two-sum.go
@@ -0,0 +1,12 @@
+package current
+
+func twoSum(nums []int, target int) []int {
+	targetMap := make(map[int]int)
+	for i, num := range nums {
+		if j, ok := targetMap[target-num]; ok {
+			return []int{i, j}
+		}
+		targetMap[num] = i
+	}
+	return nil
+}
